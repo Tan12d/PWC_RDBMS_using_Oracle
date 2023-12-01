6a032126f83145b2976ca4f896c32ce3ee26e22b
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-12-01 12:37:08 +0530

    Create sample.sql

---
+++ 3. Data Manipulation Language/Code/2. Insert Customer Records/sample.sql
@@ -0,0 +1,6 @@
+insert into Customer values( 'Andrew', 'Chicago', null, 9799874536);
+insert into Customer values( 'Holder', 'New York', 'hold@yahoo.com', 9998745318);
+insert into Customer values( 'David', 'Los Angeles', 'dav@gmail.com', 9779979979);
+insert into Customer values( 'Ronald', 'Chicago', null, 7774536287);
+insert into Customer values( 'Roy', 'Berlin', 'roy@gmail.com', 7453628745);
+insert into Customer values( 'Peter', 'New York', 'peter@yahoo.com', 9853627997);
