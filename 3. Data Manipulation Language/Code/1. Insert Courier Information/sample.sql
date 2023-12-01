330735811de484aac874db88f3329eed85922721
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-12-01 12:34:06 +0530

    Create sample.sql

---
+++ 3. Data Manipulation Language/Code/1. Insert Courier Information/sample.sql
@@ -0,0 +1,4 @@
+insert into Courier (Courier_ID, To_Address, Expected_delivery_date, Cost) values('C001', 'Los Angeles', '05-APR-2020', 500);
+insert into Courier (Courier_ID, To_Address, Expected_delivery_date, Cost) values('C002', 'Washington', '15-MAR-2020', 800);
+insert into Courier (Courier_ID, To_Address, Expected_delivery_date, Cost) values('C003', 'Berlin', '01-SEP-2020', 1000);
+
